33db9f39cd53c5310357b59f8293492b31509aa6
Craig Ford <craig_ford@icloud.com>
2022-08-10 15:30:32 -0400

    commit tree 295794693bcc034f4738091f22f778a44d0057de

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+  VALUES ('Mr. Chocolate', 4, "M", "Red", "Moody", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+  VALUES ('Rowdy', 7, "F", "Brown", "Angry", false);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+  VALUES ('Tabitha', 1, "F", "Beige", "Sweet", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+  VALUES ('Sergeant', 9, "M", "Black", "Polite", false);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+  VALUES ('Melissa', 6, "F", "Brown", "Quiet", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+  VALUES ('Grinch', 4, "M", "White", "Shy", true);  
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+  VALUES ('Wendy', 3, "F", "Black", "Moody", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+  VALUES (NULL, 3, "M", "White", "Nice", true);  
